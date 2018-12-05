fe6e1593ae643252a9c47c2b9840b411aa712f06
domagoj.salkovic <domagoj.salkovic@nanobit.co>
2018-12-05 09:26:40 +0100

    Day5

---
+++ day5.py
@@ -0,0 +1,23 @@
+import string
+
+def readFile():
+	polymer = ""
+	with open("input5.txt") as f:
+		polymer = f.readlines()
+	return polymer
+
+def destroyPolymer(polymer):
+	unitsList = []
+	for upper,lower in zip(string.ascii_uppercase,string.ascii_lowercase):
+		unitsList.append(upper+lower)
+		unitsList.append(lower+upper)
+	canDestroy = True
+	while canDestroy:
+		temp = polymer[0]
+		for unit in unitsList:
+			polymer[0] = polymer[0].replace(unit,"")
+		if polymer[0] == temp:
+			canDestroy = False
+	print len(polymer[0])
+####
+destroyPolymer(readFile())
