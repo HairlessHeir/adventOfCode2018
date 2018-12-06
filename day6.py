976f4e5d79eec8e87068ba5822be8b4bdeaf6a85
domagoj.salkovic <domagoj.salkovic@nanobit.co>
2018-12-06 14:44:58 +0100

    Day6 modified

--- day6.py
+++ day6.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from math import hypot
 
 def readFile():
 	coordinates = []
@@ -9,13 +10,23 @@ def readFile():
 	return coordinates
 
 def getClosest(coordinates, point):
-	coordinate = point.split(":")
+	coordinate = (int(point.split(":")[0]),int(point.split(":")[1]))
+	minDistance = 10000
+	closestPoints = 0
+	for current in coordinates:
+		distance = math.hypot(current[0]-coordinate[0],current[1]-coordinate[1])
+		if distance <= minDistance:
+			minDistance = distance
+			closestPoints += 1
+	if closestPoints >
 
 def makeGrid(coordinates):
 	grid = {}
 	minH,maxH,minV,maxV = 1000,0,1000,0
+	counter = 0
 	for pair in coordinates:
-		grid[str(pair[0])+":"+str(pair[1])] = "0"
+		grid[str(pair[0])+":"+str(pair[1])] = counter
+		counter += 1
 		if minH > pair[0]:
 			minH = pair[0]
 		if maxH < pair[0]:
@@ -25,12 +36,10 @@ def makeGrid(coordinates):
 		if maxV < pair[1]:
 			maxV = pair[1]	
 
-	counter = 1
 	for i in range(maxH-minH+1):
 		for j in range(maxV-minV+1):
-			grid[str(i+minH)+":"+str(j+minV)] = counter
-	for key in grid.keys():
-		result = getClosest(coordinates,key)
+			point = str(i+minH)+":"+str(j+minV)
+			grid[point] = getClosest(coordinates, point)
 	print grid
 
 makeGrid(readFile())
