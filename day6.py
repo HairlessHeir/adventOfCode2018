5b07a1e4113e051498a6307edecce05e4b660d88
domagoj.salkovic <domagoj.salkovic@nanobit.co>
2018-12-06 15:57:34 +0100

    Day6 modified more

--- day6.py
+++ day6.py
@@ -12,21 +12,25 @@ def readFile():
 def getClosest(coordinates, point):
 	coordinate = (int(point.split(":")[0]),int(point.split(":")[1]))
 	minDistance = 10000
-	closestPoints = 0
-	for current in coordinates:
-		distance = math.hypot(current[0]-coordinate[0],current[1]-coordinate[1])
+	closestPoints = []
+	closestID = 0
+	for i,current in enumerate(coordinates):
+		distance = hypot(current[0]-coordinate[0],current[1]-coordinate[1])
 		if distance <= minDistance:
 			minDistance = distance
-			closestPoints += 1
-	if closestPoints >
+			closestPoints.append(current)
+			closestID = str(i)
+
+	if len(closestPoints) > 1:
+		closestID = "."
+	return closestID
+
 
 def makeGrid(coordinates):
 	grid = {}
 	minH,maxH,minV,maxV = 1000,0,1000,0
-	counter = 0
-	for pair in coordinates:
-		grid[str(pair[0])+":"+str(pair[1])] = counter
-		counter += 1
+	for i,pair in enumerate(coordinates):
+		grid[str(pair[0])+":"+str(pair[1])] = str(i)
 		if minH > pair[0]:
 			minH = pair[0]
 		if maxH < pair[0]:
@@ -40,6 +44,7 @@ def makeGrid(coordinates):
 		for j in range(maxV-minV+1):
 			point = str(i+minH)+":"+str(j+minV)
 			grid[point] = getClosest(coordinates, point)
-	print grid
+	for value in grid.values():
+		print value
 
 makeGrid(readFile())
