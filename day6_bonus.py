afe4c2edfce8eaa0fd70febc53c1bddee465f672
domagoj.salkovic <domagoj.salkovic@nanobit.co>
2018-12-07 09:19:15 +0100

    Day6 bonus

--- day6_bonus.py
+++ day6_bonus.py
@@ -4,31 +4,25 @@ from collections import defaultdict,namedtuple
 
 point = namedtuple("point",["x","y"])
 
-def readFile():
+def readFile(fileName):
 	coordinates = []
-	with open("input6.txt") as f:
+	with open(fileName) as f:
 		for line in f.readlines():
 			coordinate = line.split(", ")
 			coordinates.append(point(int(coordinate[0]),int(coordinate[1])))
 	return coordinates
 
-#distance = hypot(current[0]-coordinate[0],current[1]-coordinate[1])
-
 def getPointValue(coordinates,currentPoint):
-    distances = {}
-    minDistance = 1000000
+    totalDistance = 0
+    maximumDistance = 10000
+
     for i,coordinate in enumerate(coordinates):
-        distances[i] = abs(coordinate[0]-currentPoint[0])+abs(coordinate[1]-currentPoint[1])
-        if distances[i]<=minDistance:
-            minDistance=distances[i]
-    count = 0
-    myKey = ""
-    for key,value in zip(distances.keys(),distances.values()):
-        if value == minDistance:
-            count += 1
-            myKey = key
-    if count > 1:
-        myKey = "."
+        totalDistance += abs(coordinate[0]-currentPoint[0])+abs(coordinate[1]-currentPoint[1])
+
+    myKey = "."
+    if totalDistance < maximumDistance:
+    	myKey = "#"
+
     return myKey
 
 def makeGrid(coordinates):
@@ -42,27 +36,29 @@ def makeGrid(coordinates):
 
     maxValue = int(max([maxH,maxV]))
     counter = 0
+    totalArea = 0
     removeKeys = []
     areaSizes = defaultdict(lambda : 1)
     for i in range(maxValue+1):
         for j in range(maxValue+1):
             currentPoint = point(j,i)
             if currentPoint in coordinates:
-                #print "+",
+                valueForCurrent = getPointValue(coordinates,currentPoint)
+                if valueForCurrent == "#":
+                	totalArea += 1
                 counter += 1
             else:
-                valueForCoordinate = getPointValue(coordinates,currentPoint)
-                if i == 0 or i == maxValue or j == 0 or j == maxValue:
-                	removeKeys.append(valueForCoordinate)
-                
-                areaSizes[valueForCoordinate]+=1
-                #print valueForCoordinate,
-    for el in removeKeys:
-    	areaSizes[el] = 0
-    maximumArea = 0
-    for el2 in areaSizes.keys():
-       	if int(areaSizes[el2])>maximumArea:
-       		maximumArea = areaSizes[el2]
-    print maximumArea
+                valueForCurrent = getPointValue(coordinates,currentPoint)
+                if valueForCurrent == "#":
+                	totalArea += 1
+    print totalArea
 
-makeGrid(readFile())
+    # for el in removeKeys:
+    # 	areaSizes[el] = 0
+    # maximumArea = 0
+    # for el2 in areaSizes.keys():
+    #    	if int(areaSizes[el2])>maximumArea:
+    #    		maximumArea = areaSizes[el2]
+    # print maximumArea
+
+makeGrid(readFile("input6.txt"))
