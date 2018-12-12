dbabfa9e8f870fa647fa4c66576e125c899d0650
HairlessHeir <domagoj.salkovic@gmail.com>
2018-12-13 00:52:13 +0100

    day12 add

--- day12.py
+++ day12.py
@@ -1,15 +1,39 @@
+from collections import defaultdict
+
 def readFile(filename):
     instructions = {}
     with open(filename) as f:
         initState = f.readline().split("state: ")[1].rstrip()
+        initStateDict = defaultdict(lambda:".")
+        for i,state in enumerate(initState):
+                initStateDict[i] = state
         f.readline()
         for line in f.readlines():
             instructions[line.split(" => ")[0]] = line.split(" => ")[1].rstrip()
-    return initState,instructions
+    return initStateDict,instructions
 
 def runSimulation(initState,instructions,generations):
+    minLeftIndex = 0
+    maxRightIndex = len(initState.keys())
     for i in range(generations):
-        print i
+        newPotValues = []
+        for k in range(len(initState.keys())):
+            j = minLeftIndex+k
+            potKey = initState[j-2]+initState[j-1]+initState[j]+initState[j+1]+initState[j+2]
+            newPotValue = instructions[potKey]
+            #print potKey," -> ",newPotValue
+            newPotValues.append(newPotValue)
+        totalSum = 0
+        for k in range(len(newPotValues)):
+            initState[minLeftIndex + k] = newPotValues.pop(0)
+            print initState[minLeftIndex+k],
+            if initState[minLeftIndex + k] == "#":
+                totalSum+=minLeftIndex + k
+        print "Gen:",i," sum:",totalSum
+        print initState
 
-initialState, instructionSet = readFile("input12.txt")
-runSimulation(initialState,instructionSet,20)
+        minLeftIndex -= 2
+        maxRightIndex += 2
+
+initialState, instructionSet = readFile("input12_test.txt")
+runSimulation(initialState,instructionSet,3)
