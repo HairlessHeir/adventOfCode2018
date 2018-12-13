053bcbbf9674849eb2755a4e778df205967760d8
domagoj.salkovic <domagoj.salkovic@nanobit.co>
2018-12-13 15:22:22 +0100

    Day12

--- day12.py
+++ day12.py
@@ -8,32 +8,36 @@ def readFile(filename):
         for i,state in enumerate(initState):
                 initStateDict[i] = state
         f.readline()
+        instructions = defaultdict(lambda:".")
         for line in f.readlines():
             instructions[line.split(" => ")[0]] = line.split(" => ")[1].rstrip()
+
     return initStateDict,instructions
 
 def runSimulation(initState,instructions,generations):
     minLeftIndex = 0
-    maxRightIndex = len(initState.keys())
+    maxRightIndex = len(initState)
     for i in range(generations):
-        newPotValues = []
-        for k in range(len(initState.keys())):
-            j = minLeftIndex+k
-            potKey = initState[j-2]+initState[j-1]+initState[j]+initState[j+1]+initState[j+2]
-            newPotValue = instructions[potKey]
-            #print potKey," -> ",newPotValue
-            newPotValues.append(newPotValue)
+        newPotValues = defaultdict(lambda:".")
+        genString = ""
+        for j in range(1,5):
+            initState[min(initState.keys())-j] = "."
+            initState[max(initState.keys())+j] = "."
+        for (k,v) in initState.items():
+            newPotValues[k] = instructions[initState[k-2]+initState[k-1]+initState[k]+initState[k+1]+initState[k+2]]
+        
+        for k in range(maxRightIndex+40):
+            genString+=initState[k]    
         totalSum = 0
-        for k in range(len(newPotValues)):
-            initState[minLeftIndex + k] = newPotValues.pop(0)
-            print initState[minLeftIndex+k],
-            if initState[minLeftIndex + k] == "#":
-                totalSum+=minLeftIndex + k
-        print "Gen:",i," sum:",totalSum
-        print initState
 
-        minLeftIndex -= 2
-        maxRightIndex += 2
+        for (k,v) in newPotValues.items():
+            initState[k] = v
+        for (k,v) in initState.items():
+            if v == "#":
+                totalSum+= k
+        newPotValues.clear()
+        #print genString
+        print i+1,totalSum
 
-initialState, instructionSet = readFile("input12_test.txt")
-runSimulation(initialState,instructionSet,3)
+initialState, instructionSet = readFile("input12.txt")
+runSimulation(initialState,instructionSet,20)
