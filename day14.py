500ca22b3e415608ff7fbe10f18dc73bbfebaa7e
domagoj.salkovic <domagoj.salkovic@nanobit.co>
2018-12-18 10:18:25 +0100

    Day14

---
+++ day14.py
@@ -0,0 +1,23 @@
+def getRecipes(recipes,indxOne,indxTwo):
+    result = recipes[indxOne] + recipes[indxTwo]
+    newRecipes = map(int,str(result))
+    for i in newRecipes:
+        recipes.append(i)
+    indxOne = (indxOne+(recipes[indxOne]+1))%(len(recipes))
+    indxTwo = (indxTwo+(recipes[indxTwo]+1))%(len(recipes))
+    return recipes,indxOne,indxTwo
+
+###
+puzzleInput = 890691
+currentRecipes,currentIndexFirst,currentIndexSecond = [3,7],0,1
+numOfIterations = 900000
+while True:
+    currentRecipes,currentIndexFirst,currentIndexSecond = getRecipes(currentRecipes,currentIndexFirst,currentIndexSecond)
+    numOfIterations -=1
+    if numOfIterations <= 0:
+        break
+recipeOffset = puzzleInput
+finalScore = ""
+for i in currentRecipes[recipeOffset:recipeOffset+10]:
+    finalScore += str(i)
+print finalScore
