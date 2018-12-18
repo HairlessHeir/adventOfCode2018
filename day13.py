91e7aab1ab6a06f638e631bc4ebdedaec368970c
domagoj.salkovic <domagoj.salkovic@nanobit.co>
2018-12-18 09:31:47 +0100

    Day13

--- day13.py
+++ day13.py
@@ -1,76 +1,120 @@
 import numpy as np
 import copy
 
+class Cart:
+    def __init__(self,cartType,x,y):
+        self.cartType = cartType
+        self.x = x
+        self.y = y
+        self.turnDirection = 0
+    def turnDirection(self):
+        return self.turnDirection
+    def setTurnDirection(self):
+        self.turnDirection += 1
+        self.turnDirection %= 3
+    def moveModifier(self):
+        modX,modY = 0,0
+        if self.cartType == "<":
+            modX,modY = -1,0
+        elif self.cartType == ">":
+            modX,modY = 1,0
+        elif self.cartType == "^":
+            modX,modY = 0,-1
+        elif self.cartType == "v":
+            modX,modY = 0,1
+        return modX,modY
+    def angleRightMoveModifier(self):
+        #for / angle
+        modX,modY = 0,0
+        if self.cartType == "<":
+            modX,modY = -1,1
+        elif self.cartType == ">":
+            modX,modY = 1,-1
+        elif self.cartType == "^":
+            modX,modY = 1,-1
+        elif self.cartType == "v":
+            modX,modY = -1,1
+        return modX,modY
+    def angleLeftMoveModifier(self):
+        #for \ angle
+        modX,modY = 0,0
+        if self.cartType == "<":
+            modX,modY = -1,-1
+        elif self.cartType == ">":
+            modX,modY = 1,1
+        elif self.cartType == "^":
+            modX,modY = -1,-1
+        elif self.cartType == "v":
+            modX,modY = 1,1
+        return modX,modY
+
 def readFile(filename):
     rows = []
     with open(filename) as f:
         for line in f.readlines():
             row = list(line.rstrip())
             rows.append(row)
+    return rows
 
-    a = np.array([rows])
-    return a
+def cartDict(cartStage):
+    cartTypes = [">","^","<","v"]
+    carts = {}
+    for i,row in enumerate(cartStage):
+            for j,el in enumerate(row):
+                if el in cartTypes:
+                    carts[len(carts)+1] = Cart(el, i, j)
+    for (k,cart) in carts.items():
+        if cart.cartType == "<":
+            cartStage[cart.x][cart.y] = "-"
+        elif cart.cartType == ">":
+            cartStage[cart.x][cart.y] = "-"
+        elif cart.cartType == "^":
+            cartStage[cart.x][cart.y] = "|"
+        elif cart.cartType == "v":
+            cartStage[cart.x][cart.y] = "|"
+
+    return carts, cartStage
+
+def printCarts(carts):
+    for (k,v) in carts.items():
+        print k,v.cartType,"(",v.x,",",v.y,")"
 
 def printTracks(cartStage):
-    print cartStage.shape
     for row in cartStage:
         print ' '.join(row)
-def getNextPosition(x,y,cartType,cartStage):
+def getNextPosition(cartStage,carts):
     cartTypes = [">","v","<","^"]
-    print cartStage
-    print cartStage.shape
-    if cartType == cartTypes[0]:
-        if cartStage[x+1,y] == "\\":
-            cartStage[x+1,y+1] = "v"
-        elif cartStage[x,y+1] == "/":
-            cartStage[x-1,y+1] = "^"
-        elif cartStage[x+1,y] == "-":
-            cartStage[x+1,y] = ">"
-        elif cartStage[x+1,y] == "+":
-            cartStage[x+1,y] = "^"
-        cartStage[x,y] = "-"
-    elif cartType == cartTypes[1]:
-        if cartStage[x,y+1] == "\\":
-            cartStage[x+1,y+1] = ">"
-        elif cartStage[x,y+1] == "/":
-            cartStage[x-1,y+1] = "<"
-        elif cartStage[x,y+1] == "|":
-            cartStage[x,y+1] = "v"
-        elif cartStage[x,y+1] == "+":
-            cartStage[x,y] = ">"
-        cartStage[x,y] = "|"
-    elif cartType == cartTypes[2]:
-        if cartStage[x-1,y] == "\\":
-            cartStage[x-1,y-1] = "^"
-        elif cartStage[x-1,y] == "/":
-            cartStage[x-1,y+1] = "v"
-        elif cartStage[x-1,y] == "-":
-            cartStage[x-1,y] = "<"
-        elif cartStage[x-1,y] == "+":
-            cartStage[x-1,y] = "v"
-        cartStage[x,y] = "-"
-    elif cartType == cartTypes[3]:
-        if cartStage[x,y-1] == "\\":
-            cartStage[x-1,y-1] = "<"
-        elif cartStage[x,y-1] == "/":
-            cartStage[x+1,y-1] = ">"
-        elif cartStage[x,y-1] == "|":
-            cartStage[x,y-1] = "^"
-        elif cartStage[x,y-1] == "+":
-            cartStage[x-1,y-1] = "<"
-        cartStage[x,y] = "|"
-    return cartStage
+    cartCrash = False
+
+    for (k,cart) in carts.items():
+        modX,modY = cart.moveModifier()
+        if cartStage[x+modX][y+modY] == "\\":
+            lX,lY = cart.angleLeftMoveModifier()
+            carts[k].x = cart.x+lX
+            carts[k].y = cart.y+lY
+
+        elif cartStage[x+modX][y+modY] == "/":
+            cartStage[x-1][y+1] = "^"
+        elif cartStage[x+modX][y+modY] == "-":
+            cartStage[x+1][y] = ">"
+        elif cartStage[x+modX][y+modY] == "+":
+            cartStage[x+1][y] = "^"
+        cartStage[x][y] = "-"
+
+    return cartStage, cartCrash
 
 
 def getCartCrash(cartStage):
     cartTypes = [">","<","v","^"]
+    carts, cartStage = cartDict(cartStage)
+    printCarts(carts)
+    printTracks(cartStage)
+    
     while True:
-        for i,row in enumerate(cartStage):
-            for j,el in enumerate(row):
-                if el in cartTypes:
-                    cartStage = getNextPosition(i, j, el, cartStage)
-
+        cartStage,cartCrash = getNextPosition(cartStage, carts)
         printTracks(cartStage)
+        if cartCrash:
+            break
     
 
 getCartCrash(readFile("input13_test.txt"))
